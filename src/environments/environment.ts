46675d20852f674c569adc119852e0d72999e620
Farzane2630 <farzanekazemi8517@gmail.com>
2025-04-07 11:17:04 +0200

    ref: api address after deploy backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_AUTH_URL: "http://localhost:5000/api/auth", // Your API base URL for development
-  API_URL: "http://localhost:5000/api", // Your API base URL for development
-  IMAGE_BASE_URL: "http://localhost:5000", // Your AVATAR API 
+  API_AUTH_URL: "https://backend-ql6a.onrender.com/api/auth", // Your API base URL for development
+  API_URL: "https://backend-ql6a.onrender.com/api", // Your API base URL for development
+  IMAGE_BASE_URL: "https://backend-ql6a.onrender.com", // Your AVATAR API 
 };
